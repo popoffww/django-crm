5e962b1f765b760964fd328265db23e5a2cecd2d
popoffww <popoffww@yandex.ru>
2024-03-13 16:35:40 +0600

    Customer Relationship Management

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name=''),
+    path('', views.home, name='home'),
     path('register', views.register, name='register'),
     path('my_login', views.my_login, name='my_login'),
     path('user_logout', views.user_logout, name='user_logout'),
